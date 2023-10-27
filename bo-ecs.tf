66efee2591f159b2743200ef9162b35161f66067
mico <michaelangeloviola@gmauil.com>
2023-10-28 01:48:14 +0800

    added ecs and sg

--- bo-ecs.tf
+++ bo-ecs.tf
@@ -15,7 +15,7 @@ module "bo-ecs-module" {
   vswitch_id                  = module.vpc.vswitch_ids[1]
   security_group_ids          = [alicloud_security_group.bo-sg.id]
   associate_public_ip_address = false
-  password = random_password.password.result
+  password = random_password.password2.result
 
   system_disk_category = "cloud_essd"
   system_disk_size     = 100
@@ -29,7 +29,7 @@ module "bo-ecs-module" {
 resource "alicloud_kms_secret" "default2" {
   secret_name                   = "secretfoo"
   description                   = "from terraform"
-  secret_data                   = "${random_password.password.result}"
+  secret_data                   = "${random_password.password2.result}"
   version_id                    = "000000000001"
   force_delete_without_recovery = true
   secret_type = "ECS"
