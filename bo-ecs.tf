ea0778296845369b41426f06b72e69a13721d124
mico <michaelangeloviola@gmauil.com>
2023-10-28 00:33:56 +0800

    added ecs and sg

--- bo-ecs.tf
+++ bo-ecs.tf
@@ -11,7 +11,7 @@ module "bo-ecs-module" {
   name                        = "${var.env_name}-${var.project}-bo"
   use_num_suffix              = true
   image_id                    = data.alicloud_images.ubuntu.ids.0
-  instance_type               = "ecs.g8a.large"
+  instance_type               = "ecs.g7.large"
   vswitch_id                  = module.vpc.vswitch_ids[1]
   security_group_ids          = [alicloud_security_group.bo-sg.id]
   associate_public_ip_address = false
