b3acea4aae80733225ff678194acbed3ba0acad5
mico <michaelangeloviola@gmauil.com>
2023-10-28 00:23:27 +0800

    added ecs and sg

--- bo-sg.tf
+++ bo-sg.tf
@@ -1,36 +1,15 @@
-module "bo-sg-module" {
-  source  = "alibaba/security-group/alicloud"
+resource "alicloud_security_group" "bo-sg" {
+  name        = "bo-sg"
+  description = "bo-sg security group"
+}
 
-  name        = "${var.env_name}-${var.project}-bo-sg"
-  description = "Security group for bo custom ports open within VPC"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_cidr_blocks      = [var.vpc_cidr]
-  ingress_rules            = ["https-443-http-8080"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "https"
-      description = "User-service ports"
-      cidr_blocks = var.vpc_cidr
-    },
-    {
-      from_port   = 8080
-      to_port     = 8080
-      protocol    = "http"
-      description = "User-service ports"
-      cidr_blocks = var.vpc_cidr
-    }
-  ]
-  egress_cidr_blocks      = [var.vpc_cidr]
-  egress_with_cidr_blocks = [
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "https"
-      description = "User-service ports"
-      cidr_blocks = var.vpc_cidr
-    }
-  ]
+resource "alicloud_security_group" "bo-sg-rule" {
+  name = "bo-sg-rule"
+}
+resource "alicloud_security_group_rule" "https" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  port_range        = "443"
+  security_group_id = alicloud_security_group.bo-sg.id
+  cidr_ip           = var.vpc_cidr
 }
