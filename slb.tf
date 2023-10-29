54408ea7c5377953d96a3ff5950fdc7a77e4e158
Paolo <paolo@cloudmade.io>
2023-10-30 01:58:50 +0800

    added ALB

--- slb.tf
+++ slb.tf
@@ -1,10 +1,22 @@
-resource "alicloud_slb_load_balancer" "load_balancer" {
-  load_balancer_name = "${var.env_name}-${var.project}-intra-lb"
-  address_type       = "intranet"
-  load_balancer_spec = "slb.s3.large"
-  vswitch_id         = module.vpc.vswitch_ids[0]
+data "alicloud_alb_zones" "default" {}
+
+resource "alicloud_alb_load_balancer" "default" {
+  vpc_id                 = module.vpc.vpc_id
+  address_type           = "Intranet"
+  address_allocated_mode = "Fixed"
+  load_balancer_name     = "${var.env_name}-${var.project}-intra-lb"
+  load_balancer_edition  = "WAF Enabled"
+  load_balancer_billing_config {
+    pay_type = "PayAsYouGo"
+  }
   tags = {
-    info = "create for intranet"
     name = "${var.env_name}-${var.project}-intra-lb"
+  }
+  zone_mappings {
+    vswitch_id = module.vpc.vswitch_ids[0]
+    zone_id    = data.alicloud_alb_zones.default.zones.0.id
+  }
+  modification_protection_config {
+    status = "NonProtection"
   }
 }
