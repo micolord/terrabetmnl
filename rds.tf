54dd8b7067535cec807ea9c3e4ca9ad90a618e4f
Paolo <paolo@cloudmade.io>
2023-10-30 03:49:47 +0800

    deploy DB

--- rds.tf
+++ rds.tf
@@ -1,10 +1,12 @@
-resource "alicloud_db_instance" "default" {
+data "alicloud_db_zones" "default" {}
 
+resource "alicloud_db_instance" "default" {
   engine                   = "MariaDB"
   engine_version           = "10.3"
   instance_type            = "mariadb.x4.large.2c"
   instance_storage         = "200"
   instance_name            = "${var.env_name}-${var.project}-master-rds"
+  zone_id                  = data.alicloud_db_zones.default.zones.0.id
   vswitch_id               = module.vpc.vswitch_ids[3]
   monitoring_period        = "60"
   db_instance_storage_type = "cloud_essd"
