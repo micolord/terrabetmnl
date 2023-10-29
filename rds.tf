a77cfef43095d7859db2939ed62e7eb7ad242871
Paolo <paolo@cloudmade.io>
2023-10-30 04:10:42 +0800

    deploy DB

--- rds.tf
+++ rds.tf
@@ -1,8 +1,10 @@
 data "alicloud_db_zones" "default" {}
 
 resource "alicloud_db_instance" "default" {
-  engine                   = "MariaDB"
-  engine_version           = "10.3"
+  #engine                   = "MariaDB"
+  #engine_version           = "10.3"
+  engine                   = "MySQL"
+  engine_version           = "8.0"
   instance_type            = "mariadb.x4.large.2c"
   instance_storage         = "200"
   instance_name            = "${var.env_name}-${var.project}-master-rds"
