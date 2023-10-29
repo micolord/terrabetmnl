728e916600ad0bce70af7ed2305e5cdd694d5a0a
Paolo <paolo@cloudmade.io>
2023-10-30 01:44:09 +0800

    added LB

--- slb.tf
+++ slb.tf
@@ -1,7 +1,7 @@
 resource "alicloud_slb_load_balancer" "load_balancer" {
   load_balancer_name = "${var.env_name}-${var.project}-intra-lb"
   address_type       = "intranet"
-  load_balancer_spec = "slb.s4.large"
+  load_balancer_spec = "slb.s3.large"
   vswitch_id         = module.vpc.vswitch_ids[0]
   tags = {
     info = "create for intranet"
