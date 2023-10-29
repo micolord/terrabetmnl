587d47c10aab48e860d212d88eb598b80299964c
mico <michaelangeloviola@gmauil.com>
2023-10-29 23:56:38 +0800

    added password

--- bo-ecs.tf
+++ bo-ecs.tf
@@ -7,12 +7,15 @@ resource "alicloud_instance" "ecs_instance" {
   instance_name        = "${var.env_name}-${var.project}-bo"
   image_id             = data.alicloud_images.ubuntu.ids.0
   instance_type        = "ecs.g7.large"
-  security_group_id    = [alicloud_security_group.bo-sg.id]
+  security_groups      = [alicloud_security_group.bo-sg.id]
   vswitch_id           = module.vpc.vswitch_ids[1]
   password             = "dynamic_random_password"
   system_disk_category = "cloud_essd"
   system_disk_size     = 100
-  instance_tags = {
+  tags = {
+    Name = "${var.env_name}-${var.project}-bo"
+  }
+  volume_tags = {
     Name = "${var.env_name}-${var.project}-bo"
   }
 }
