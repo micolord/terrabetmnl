a0e18dba696442df92cb2e1513772cb0b948bf59
Paolo <paolo@cloudmade.io>
2023-10-30 02:09:36 +0800

    added ALB internet

--- slb.tf
+++ slb.tf
@@ -2,7 +2,7 @@ data "alicloud_alb_zones" "default" {}
 
 resource "alicloud_alb_load_balancer" "default" {
   vpc_id                 = module.vpc.vpc_id
-  address_type           = "Intranet"
+  address_type           = "Internet"
   address_allocated_mode = "Fixed"
   load_balancer_name     = "${var.env_name}-${var.project}-intra-lb"
   load_balancer_edition  = "StandardWithWaf"
