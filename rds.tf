46111a13bd630ded900db08e5c199e8d04421e78
Paolo <paolo@cloudmade.io>
2023-11-05 20:21:26 +0800

    remove read replica

--- rds.tf
+++ rds.tf
@@ -9,23 +9,20 @@ resource "alicloud_db_instance" "default" {
   instance_storage         = "200"
   instance_name            = "${var.env_name}-${var.project}-master-rds"
   zone_id                  = data.alicloud_db_zones.default.zones.0.id
-  #zone_id                  = "zone-a"
   vswitch_id               = module.vpc.vswitch_ids[3]
   monitoring_period        = "60"
   db_instance_storage_type = "cloud_essd"
-  #security_group_ids       = [alicloud_security_group.rds-sg.id]
   security_ips             = [var.vpc_cidr]
   category                 = "Basic"
-  #master_username          = "apsarauser"
-  #master_user_password     = "dynamic_random_password"
+
 }
 
-resource "alicloud_db_readonly_instance" "default" {
-  zone_id               = data.alicloud_db_zones.default.zones.0.id
-  master_db_instance_id = alicloud_db_instance.default.id
-  engine_version        = alicloud_db_instance.default.engine_version
-  instance_storage      = alicloud_db_instance.default.instance_storage
-  instance_type         = alicloud_db_instance.default.instance_type
-  instance_name         = "${var.env_name}-${var.project}-readonly"
-  vswitch_id            = module.vpc.vswitch_ids[3]
-}
+#resource "alicloud_db_readonly_instance" "default" {
+#  zone_id               = data.alicloud_db_zones.default.zones.0.id
+#  master_db_instance_id = alicloud_db_instance.default.id
+#  engine_version        = alicloud_db_instance.default.engine_version
+#  instance_storage      = alicloud_db_instance.default.instance_storage
+#  instance_type         = alicloud_db_instance.default.instance_type
+#  instance_name         = "${var.env_name}-${var.project}-readonly"
+#  vswitch_id            = module.vpc.vswitch_ids[3]
+#}
