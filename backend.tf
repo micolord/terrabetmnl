6e3b9face84f03594de51bcd0b42b357457a43a6
Paolo <paolo@cloudmade.io>
2023-11-21 00:02:37 +0800

    update switch routetable

--- backend.tf
+++ backend.tf
@@ -4,4 +4,12 @@ terraform {
     key   = "dynamic_env-state.tfstate"
     region = "ap-southeast-6"
   }
+
+  required_providers {
+    alicloud = {
+      source = "aliyun/alicloud"
+      version = "1.212.0"
+    }
+  }
+
 }
