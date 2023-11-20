3721153ae3e3d0237735b88d46d9fed0eb9ae80e
Paolo <paolo@cloudmade.io>
2023-11-21 00:24:42 +0800

    revert providers

--- vpc.tf
+++ vpc.tf
@@ -82,7 +82,7 @@ resource "alicloud_eip_association" "int_nat_assoc2" {
 }
 
 resource "alicloud_snat_entry" "int_nat_snat2" {
-  snat_table_id     = alicloud_nat_gateway.int_nat_gw2.id
+  snat_table_id     = alicloud_nat_gateway.int_nat_gw2.snat_table_ids
   source_vswitch_id = module.vpc.vswitch_ids[2]
   snat_ip           = alicloud_eip_address.eip_addr_snat2.ip_address
 }
