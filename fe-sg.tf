083c66627d75aec2402195aa91ce797f065b2a32
Paolo <peraltadavidpaolo@gmail.com>
2024-02-19 02:02:37 +0800

    opened port 80 and removed 443. added dns ports tcp/udp

--- fe-sg.tf
+++ fe-sg.tf
@@ -7,7 +7,7 @@ resource "alicloud_security_group" "fe-sg" {
 resource "alicloud_security_group_rule" "fe-https" {
   type              = "ingress"
   ip_protocol       = "tcp"
-  port_range        = "443/443"
+  port_range        = "80/80"
   security_group_id = alicloud_security_group.fe-sg.id
   cidr_ip           = var.vpc_cidr
 }
@@ -16,6 +16,22 @@ resource "alicloud_security_group_rule" "fe-https-egress" {
   type              = "egress"
   ip_protocol       = "tcp"
   port_range        = "443/443"
-  security_group_id = alicloud_security_group.bo-sg.id
+  security_group_id = alicloud_security_group.fe-sg.id
   cidr_ip           = var.vpc_cidr
+}
+
+resource "alicloud_security_group_rule" "fe-udp-dns-egress" {
+  type              = "egress"
+  ip_protocol       = "udp"
+  port_range        = "53/53"
+  security_group_id = alicloud_security_group.fe-sg.id
+  cidr_ip           = "0.0.0.0/0"
+}
+
+resource "alicloud_security_group_rule" "fe-tcp-dns-egress" {
+  type              = "egress"
+  ip_protocol       = "tcp"
+  port_range        = "53/53"
+  security_group_id = alicloud_security_group.fe-sg.id
+  cidr_ip           = "0.0.0.0/0"
 }
--- jobproc-sg.tf
+++ jobproc-sg.tf
@@ -7,7 +7,7 @@ resource "alicloud_security_group" "jobproc-sg" {
 resource "alicloud_security_group_rule" "jobproc-https" {
   type              = "ingress"
   ip_protocol       = "tcp"
-  port_range        = "443/443"
+  port_range        = "80/80"
   security_group_id = alicloud_security_group.jobproc-sg.id
   cidr_ip           = var.vpc_cidr
 }
@@ -16,6 +16,22 @@ resource "alicloud_security_group_rule" "jobproc-https-egress" {
   type              = "egress"
   ip_protocol       = "tcp"
   port_range        = "443/443"
-  security_group_id = alicloud_security_group.bo-sg.id
+  security_group_id = alicloud_security_group.jobproc-sg.id
   cidr_ip           = var.vpc_cidr
+}
+
+resource "alicloud_security_group_rule" "jobproc-udp-dns-egress" {
+  type              = "egress"
+  ip_protocol       = "udp"
+  port_range        = "53/53"
+  security_group_id = alicloud_security_group.jobproc-sg.id
+  cidr_ip           = "0.0.0.0/0"
+}
+
+resource "alicloud_security_group_rule" "jobproc-tcp-dns-egress" {
+  type              = "egress"
+  ip_protocol       = "tcp"
+  port_range        = "53/53"
+  security_group_id = alicloud_security_group.jobproc-sg.id
+  cidr_ip           = "0.0.0.0/0"
 }
